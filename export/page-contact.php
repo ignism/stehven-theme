add69d31af79d192a0684892649c0e883d539060
Mark Brand <markbrand@Marks-iMac.fritz.box>
2019-02-05 17:17:54 +0100

    removal of export

--- export/page-contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Template Name: Contact Page */
-
-$context = Timber::get_context();
-$context['page'] = new TimberPost();
-// $context['locale'] = get_locale();
-$templates = array( 'pages/contact.twig' );
-Timber::render( $templates, $context);
